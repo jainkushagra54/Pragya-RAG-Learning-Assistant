4815d91b27a8e226cf3fa3b2c073749e4320f617
Kushagra Jain <kushagrajaingaurya54@gmail.com>
2026-05-11 01:01:23 +0530

    Fixing typos

--- backend/rag/get_embedding_function.py
+++ backend/rag/get_embedding_function.py
@@ -3,6 +3,6 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 def get_embedding_function():
     return GoogleGenerativeAIEmbeddings(
-        model="models/text-embedding-004",
+        model="text-embedding-004",
         google_api_key=os.getenv("API_KEY")
     )
